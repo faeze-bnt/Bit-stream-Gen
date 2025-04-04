0c86a35b0dc61fef039065aa388b8cb77d6d221d
Fazi <banitaba.fzs@gmail.com>
2025-04-04 17:33:38 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+
+def Fugen_exact (Bit_Width, Parallel_deg, Input_number):
+    Inp_value = int(Input_number*np.power(2, Bit_Width))
+    #print ("Input_value", Inp_value)
+    outp = np.zeros((Parallel_deg, int(np.power(2,Bit_Width)/Parallel_deg)), dtype=float, order='C')
+
+    for i in range (len(outp[0])):
+        if (Inp_value >= Parallel_deg):
+            outp[:, i] = 1
+            Inp_value-= Parallel_deg
+        else:
+            for ii in range (Parallel_deg):
+                if(Inp_value > 0):
+                    outp[ii, i] = 1
+                    Inp_value -= 1
+            break
+    return outp
+
+
+
+        
+    
---
+++ SNG.m
@@ -0,0 +1,13 @@
+function Stream = SNG(FP_input, StreamLength)
+
+%Create an array of numbers from 0 to StreamLength
+Data_Set = linspace(0,StreamLength-1,StreamLength);
+
+
+%Psudo randomly sample an array with the same size
+%for different streams this will give uncorrelated streams.
+Sample = datasample(Data_Set,StreamLength,'Replace',false);
+
+
+Stream = Sample < FP_input * StreamLength; %Generate stream with corresponding maximum period LFSR
+
