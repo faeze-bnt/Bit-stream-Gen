bd2d64fb78d286b1698225e4e62385d4f6c5582a
Fazi <banitaba.fzs@gmail.com>
2025-04-09 17:01:59 -0500

    Update MedianFilter3x3.m

--- Median Filter/MedianFilter3x3.m
+++ Median Filter/MedianFilter3x3.m
@@ -2,15 +2,15 @@ function [Out_Expected, Out_Stochastic] = MedianFilter3x3(In,Stream_Length,Paral
 
 
 
-Stream_In_1 =  Fugen_approx(In(1),Stream_Length,Parallel_deg);
-Stream_In_2 =  Fugen_approx(In(2),Stream_Length,Parallel_deg);
-Stream_In_3 =  Fugen_approx(In(3),Stream_Length,Parallel_deg);
-Stream_In_4 =  Fugen_approx(In(4),Stream_Length,Parallel_deg);
-Stream_In_5 =  Fugen_approx(In(5),Stream_Length,Parallel_deg);
-Stream_In_6 =  Fugen_approx(In(6),Stream_Length,Parallel_deg);
-Stream_In_7 =  Fugen_approx(In(7),Stream_Length,Parallel_deg);
-Stream_In_8 =  Fugen_approx(In(8),Stream_Length,Parallel_deg);
-Stream_In_9 =  Fugen_approx(In(9),Stream_Length,Parallel_deg);
+Stream_In_1 =  design_approx(In(1),Stream_Length,Parallel_deg);
+Stream_In_2 =  design_approx(In(2),Stream_Length,Parallel_deg);
+Stream_In_3 =  design_approx(In(3),Stream_Length,Parallel_deg);
+Stream_In_4 =  design_approx(In(4),Stream_Length,Parallel_deg);
+Stream_In_5 =  design_approx(In(5),Stream_Length,Parallel_deg);
+Stream_In_6 =  design_approx(In(6),Stream_Length,Parallel_deg);
+Stream_In_7 =  design_approx(In(7),Stream_Length,Parallel_deg);
+Stream_In_8 =  design_approx(In(8),Stream_Length,Parallel_deg);
+Stream_In_9 =  design_approx(In(9),Stream_Length,Parallel_deg);
 
 [Min_Sort1,Max_Sort1] = Sort(Stream_In_1,Stream_In_2);
 [Min_Sort2,Max_Sort2] = Sort(Stream_In_3,Stream_In_4);
